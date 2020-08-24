7e81bcff3e0a0f02fa16e5ece7f0b4237bf8af87
Busnellinho <fabio.couto-terceiro@cea.com.br>
2020-08-24 16:10:54 -0300

    correção de rotas

--- node/db/knex.js
+++ node/db/knex.js
@@ -1,21 +1,21 @@
-const knex = require('knex')({
+// const knex = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host : '127.0.0.1',
+//       user : 'bingo',
+//       password : 'Busnello@123',
+//       database : 'bingo'
+//     }
+//   });
+
+  const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      user : 'bingo',
-      password : 'Busnello@123',
+      host : 'mysqlsrv',
+      user : 'root',
+      password : 'root',
       database : 'bingo'
     }
   });
-
-  // const knex = require('knex')({
-  //   client: 'mysql',
-  //   connection: {
-  //     host : 'mysqlsrv',
-  //     user : 'root',
-  //     password : 'root',
-  //     database : 'bingo'
-  //   }
-  // });
 
 module.exports = knex
--- node/middlewares/middleware1.js
+++ node/middlewares/middleware1.js
@@ -1,12 +1,7 @@
 const { nextTick } = require("process")
 
 const action = (req, res, next) => {
-  const host = req.headers.host
-  if( !host.includes('loto') && !host.includes('localhost')){
-    res.redirect('https://loto.bigbig73.net.br')
-  }else{
-    next()
-  }  
+  next() 
 }
 module.exports = {
   route: '*',
