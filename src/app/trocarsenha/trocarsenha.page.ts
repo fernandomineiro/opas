668c5e4bc27bfb37ead47557cb70e03cd9de58c7
Fernando Fernandes <fernandofitilan@hotmail.com>
2020-08-25 18:02:02 -0300

    arrumando a loja

--- src/app/trocarsenha/trocarsenha.page.ts
+++ src/app/trocarsenha/trocarsenha.page.ts
@@ -20,35 +20,36 @@ export class TrocarsenhaPage implements OnInit {
   }
 
   ngOnInit() {
-    this.telefone = config.telefone; 
+    this.telefone = localStorage.getItem("telefone");
     console.log(this.telefone);
   }
 
   submitForm() {
     if(this.data.senha != null){
-      if(this.data.novasenha != this.data.repetesenha){
-        console.log('senha diferentes');
-      }
-      else{
+     
+      
         this.apiService.encontrasenha(this.data.senha).subscribe((response)=>{
           if(response == null){
-            console.log('senha não encontrada');
+            alert('senha não encontrada');
           }else{
              this.apiService.alterasenha(this.telefone, this.data.novasenha).subscribe((response)=>{
         if(response == 1){
-          console.log('sucesso');
+          alert('sucesso');
+          this.router.navigate(['central']);
+        }else{
+          alert('erro');
         }
       })
           }
         })
-      }
+      
 
      // this.apiService.alterasenha(this.telefone, this.data.senha).subscribe((response)=>{
       //  console.log(response);
       //})
     }
     else{
-      console.log('Deve preencher o campo senha');
+      alert('Deve preencher o campo senha');
     }
   }
 }
